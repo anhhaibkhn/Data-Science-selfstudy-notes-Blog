9ef5b0dce45023289435fb50fac683324ca6a32e
Nguyen Hai <nguyenngochaibkhn@gmail.com>
2022-08-08 15:29:44 +0900

    Update About Me page.

---
+++ _action_files/helpers.py
@@ -0,0 +1,25 @@
+import os
+
+
+def getDirectoryList(path):
+    """ Get all subfolders that contain jupyter notebooks """
+    directoryList = []
+
+    #return nothing if path is a file
+    if os.path.isfile(path):
+        return []
+
+    #add dir to directorylist if it contains .ipynb files
+    if len([f for f in os.listdir(path) if f.endswith('.ipynb')])>0:
+        directoryList.append(path)
+
+    for d in os.listdir(path):
+        new_path = os.path.join(path, d)
+        if os.path.isdir(new_path):
+            directoryList += getDirectoryList(new_path)
+
+    return directoryList
+
+# src_dir = os.getcwd() + '\_notebooks'
+# print(src_dir)
+# print(list( (f + '\\*.ipynb') for f in getDirectoryList(src_dir)))
